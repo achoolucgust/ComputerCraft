f0907033c2b59538a207cd5363c479c3e139d64b
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-03-09 14:11:14 +0800

    finish stripmine.lua

--- turtle/stripmine.lua
+++ turtle/stripmine.lua
@@ -1,10 +1,43 @@
+function isSlot(slot,name)
+    details = textutils.serialise(turtle.getItemDetail(slot))
+    if details.name.lower().split(":")[2] == name.lower() then
+        return true
+    end
+    return false
+end
+function unclog(slot,min,max)
+    for i = min,max do
+        if isSlot(i,"air") then
+            turtle.select(slot)
+            turtle.transferTo(i)
+            return true
+        end
+    end
+    return false
+end
+function noerror(func)
+    return pcall(function()func()end)
+end
 while true do
     os.sleep(1)
     term.clear()
 
     local fuel = turtle.getFuelLevel()
-    local fuelCompareSlot = 16
-    local fuel = 
+    local maxFuel = turtle.getFuelLimit()
+    local maxSlots = 16
+    local fuelSlot = 1
+    local fuelPercentage = math.floor((fuel/maxFuel)*10)/10
 
-    print()
-end 
+    term.write(fuelPercentage.." fuel left.")
+    if fuelPercentage < 2 then
+        term.write("refueling because "..fuelPercentage.." left")
+        turtle.refuel()
+        term.write("REFUELED!")
+    end
+    if not isSlot(fuelSlot,"coal") then
+        unclog(1,2,maxSlots)
+    end
+    noerror(turtle.dig())
+    noerror(turtle.forward())
+    noerror(turtle.digDown())
+end
