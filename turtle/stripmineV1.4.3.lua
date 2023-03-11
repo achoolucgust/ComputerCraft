811769cf8a12621efc8a9d19efbc66d68750e229
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-03-12 04:47:34 +0800

    fixes, fixes, fixes.

--- turtle/stripmineV1.4.3.lua
+++ turtle/stripmineV1.4.3.lua
@@ -8,11 +8,13 @@ function split(inputstr, sep)
         end
         return t
 end
-function isSlot(slot,name)
+function isSlot(slot,name) -- WORKS probably. -- doesnt work
     turtle.select(slot)
     details = turtle.getItemDetail()
-    if details and details["name"]:lower() == name:lower() then
-        return true
+    if details then
+        if details["name"] == name then
+            return true
+        end
     end
     return false
 end
@@ -23,44 +25,59 @@ function refuel(min,max)
     end
     return false
 end
+function get_mineral() -- for that one mineral chances mod.
+    noerror(turtle.suckDown())
+    return false
+end
+function drop_useless(min,max) -- Probably works now! ignore next few lines. -- dont use, i have no idea how to check the details name.
+    for i = min,max do
+        turtle.select(i)
+        if isSlot(i,"minecraft:cobblestone") then
+            turtle.dropUp()
+        end
+    end
+    return false
+end
 function noerror(func)
     return pcall(function()func()end)
 end
 local counter = 1
 while true do
     term.clear()
-
+    
     local fuel = turtle.getFuelLevel()
     local maxFuel = turtle.getFuelLimit()
-    local maxSlots = 16
-    local fuelSlot = 1
     local fuelPercentage = math.floor((fuel/maxFuel))
     print(fuel,"out of",maxFuel)
     print(fuelPercentage.."% fuel left.")
+    get_mineral()
     if fuel < 25 then
         print("refueling because "..fuelPercentage.."% left")
         refuel(1,16)
         print("REFUELED!")
     end
-    print()
+    print("Action: "..counter)
     if counter == 1 then
-        noerror(turtle.dig())
+        noerror(turtle.dig)
     elseif counter == 2 then
-        noerror(turtle.forward())
+        noerror(turtle.forward)
     elseif counter == 3 then
-        noerror(turtle.digDown())
+        noerror(turtle.digDown)
     elseif counter == 4 then
-        noerror(turtle.turnLeft())
+        noerror(turtle.turnLeft)
     elseif counter == 5 then
-        noerror(turtle.dig())
+        noerror(turtle.dig)
     elseif counter == 6 then
-        noerror(turtle.turnRight())
+        noerror(turtle.turnRight)
     elseif counter == 7 then
-        noerror(turtle.turnRight())
+        noerror(turtle.turnRight)
     elseif counter == 8 then
-        noerror(turtle.dig())
+        noerror(turtle.dig)
     elseif counter == 9 then
-        noerror(turtle.turnLeft())
+        noerror(turtle.turnLeft)
+        counter = 0
+    end
+    if counter > 9 then
         counter = 0
     end
     counter = counter + 1
